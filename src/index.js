cbf4d95da1883143034b17424c5347c854b03c7d
Carlos Franco <carlosfranco6456@gmail.com>
2025-05-28 00:39:24 -0500

    port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+// const app = require("./app");
+
+// const port = process.env.PORT || 3001;
+
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
 const app = require("./app");
 
-const port = 3001
+const PORT = process.env.PORT || 3001;
 
-
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`✅ Server running on port ${PORT}`);
+});
